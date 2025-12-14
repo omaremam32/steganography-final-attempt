0e05a456eb1fe361314e5198ac2d647a2252ddb6
omaremam32 <omaromohamed2003@gmail.com>
2025-12-14 21:14:04 +0000

    Update import os.py
    """
        Writes a bytearray back to a file in binary mode.
        Used to save the stego-image.
        """

--- import os.py
+++ import os.py
@@ -7,3 +7,6 @@ def file_exists(path: str) -> bool:
 def read_all_bytes(path: str) -> bytearray:
     with open(path, "rb") as f:
         return bytearray(f.read())
+    def write_all_bytes(path: str, data: bytearray) -> None:
+    with open(path, "wb") as f:
+        f.write(data)
