3f2a9da34f8c91a2d20b964be2e173d53daa8b86
omaremam32 <omaromohamed2003@gmail.com>
2026-05-10 20:10:02 +0100

    Checks if the BMP format is unsupported.

--- steganography_lsb_basic.py
+++ steganography_lsb_basic.py
@@ -37,3 +37,4 @@ def file_exists(path: str) -> bool:
                                     return (False, "Not a BMP file (missing 'BM' signature).", BMP_HEADER_SIZE, None, None)
                                 pixel_offset = get_bmp_pixel_data_offset(image_data)
                                 dib_size = int.from_bytes(image_data[14:18], "little")
+                                if dib_size < 40:
