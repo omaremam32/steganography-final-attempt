29af63112fe9939f6ef9f809b7e41aede4db6fb5
omaremam32 <omaromohamed2003@gmail.com>
2026-05-10 20:04:40 +0100

    Defines a function to find where the actual pixel data starts.

--- steganography_lsb_basic.py
+++ steganography_lsb_basic.py
@@ -25,3 +25,4 @@ def file_exists(path: str) -> bool:
                                 byte_str = "".join("1" if b else "0" for b in byte_bits)
                                 chars.append(chr(int(byte_str, 2)))
                                 return "".join(chars)
+                            def get_bmp_pixel_data_offset(image_data: bytearray) -> int:
